6a0a28292fe9b493a1abd6b63d09c700e58e4adc
Sable405 <111007485+Sable405@users.noreply.github.com>
2022-08-18 11:05:09 -0700

    Input stuff

--- Number Guessing Game/Assets/Scripts/Number.cs
+++ Number Guessing Game/Assets/Scripts/Number.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Number : MonoBehaviour
 {
-   public int guess; 
+   public int guess = 5; 
    public int minValue = 1; 
    public int maxValue = 10;
 
@@ -12,12 +12,28 @@ public class Number : MonoBehaviour
     void Start()
     {
         Debug.Log("Welcome to our Numbah Guessing Game.");
-        Debug.Log("The number range is from " + minValue + "and" + maxValue);
+        Debug.Log("The number range is from " + minValue + " and " + maxValue);
+        Debug.Log("Guess a Number Using The Arrow Keys");
+        Debug.Log("Push Up arrow = Higher, Push Down arrow = Lower, Push enter = Correct");
+        Debug.Log("Tell me if your number is higher or lower : " + guess + "?");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            minValue = guess; 
+            guess = (maxValue + minValue) / 2;
+            Debug.Log("Is thine number higher or lower than: " + guess);
+        }
+        if(Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            maxValue = guess;
+            guess = (maxValue + minValue) / 2;
+            Debug.Log("Is the majesty's numbah higher or lower than: " + guess);
+
+        }
     }
+
 }
