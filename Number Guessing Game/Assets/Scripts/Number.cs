c382ce8295392bd24ce917f849596926c725cd01
Sable405 <111007485+Sable405@users.noreply.github.com>
2022-08-19 11:29:04 -0700

    Game ending

--- Number Guessing Game/Assets/Scripts/Number.cs
+++ Number Guessing Game/Assets/Scripts/Number.cs
@@ -11,15 +11,11 @@ public class Number : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Welcome to our Numbah Guessing Game.");
-        Debug.Log("The number range is from " + minValue + " and " + maxValue);
-        Debug.Log("Guess a Number Using The Arrow Keys");
-        Debug.Log("Push Up arrow = Higher, Push Down arrow = Lower, Push enter = Correct");
-        Debug.Log("Tell me if your number is higher or lower : " + guess + "?");
+       StartGame();
     }
 
     // Update is called once per frame
-    void Update()
+    async void Update()
     {
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
@@ -32,8 +28,29 @@ public class Number : MonoBehaviour
             maxValue = guess;
             guess = (maxValue + minValue) / 2;
             Debug.Log("Is the majesty's numbah higher or lower than: " + guess);
-
+            
         }
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Congrats we got yo numbah foo, which was " + guess + " we will take over the world!");
+            Debug.Log("");
+            StartGame();
+        }
+        
+    
+    }
+
+    void StartGame()
+    {
+        guess = 5;
+        minValue = 1;
+        maxValue = 10;
+         Debug.Log("Welcome to our Numbah Guessing Game.");
+        Debug.Log("The number range is from " + minValue + " and " + maxValue);
+        Debug.Log("Guess a Number Using The Arrow Keys");
+        Debug.Log("Push Up arrow = Higher, Push Down arrow = Lower, Push enter = Correct");
+        Debug.Log("Tell me if your number is higher or lower : " + guess + "?");
+        maxValue = maxValue + 1; 
     }
 
 }
